08fe8185250e4617c630e01c13f1612dba585b4a
Alana <britoalanac@gmail.com>
2025-10-07 01:35:07 -0300

    chore(structure): add initial project folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
+import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-900">
-      <h1 className="text-3xl font-bold text-orange-500 underline">
-        Tailwind Configurado com Sucesso!
-      </h1>
-    </div>
+    <LoginPage />
   )
 }
 
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-orange-500 underline">
+        Tailwind Instalado com Sucesso!
+      </h1>
+    </div>
+  )
+}
