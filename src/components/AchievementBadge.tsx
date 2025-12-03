15d94e5059c31eb4f04e0dca4200b5a015caf6bc
Alana <britoalanac@gmail.com>
2025-12-03 04:29:11 -0300

    style(components): refina componentes visuais de conquistas e progresso

--- src/components/AchievementBadge.tsx
+++ src/components/AchievementBadge.tsx
@@ -1,5 +1,5 @@
 import { type LucideIcon } from "lucide-react";
-import { Card } from "../components/ui/card";
+import { Card } from "@/components/ui/card";
 
 interface AchievementBadgeProps {
   icon: LucideIcon;
